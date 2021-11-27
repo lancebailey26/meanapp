5b9e886f9bf946cc1dd1ec3f93d2ed6bc15a0817
Lance Bailey <lancebailey222@gmail.com>
2021-11-26 20:02:11 -0500

    adding posts working, awaiting db

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,13 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-post-create',
-    templateUrl: './post-create.component.html' 
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent{
-    newPost = 'Fake me out taco bell';
-    enteredValue ='';
+    enteredContent ='';
+    enteredTitle='';
+    @Output() postCreated = new EventEmitter();
     onAddPost() {
-        this.newPost = this.enteredValue;
+        const post = {title:this.enteredTitle, 
+                    content:this.enteredContent}
+                    this.postCreated.emit(post)
     }
+    
 }

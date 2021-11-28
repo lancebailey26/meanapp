237991bdab5d77509e933977a42c92ba282775e1
Lance Bailey <lancebailey222@gmail.com>
2021-11-28 17:53:16 -0500

    form validation and error msg

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { Post } from "../post.model";
 @Component({
     selector: 'app-post-create',
@@ -9,9 +10,12 @@ export class PostCreateComponent{
     enteredContent ='';
     enteredTitle='';
     @Output() postCreated = new EventEmitter<Post>();
-    onAddPost() {
-        const post: Post = {title:this.enteredTitle, 
-                    content:this.enteredContent}
+    onAddPost(form: NgForm) {
+        if (form.invalid){
+            return
+        }
+        const post: Post = {title:form.value.title, 
+                    content:form.value.content}
                     this.postCreated.emit(post)
     }
     

0cf106f5e5952fc739b505502ef150a13232d987
Lance Bailey <lancebailey@lances-air.lan>
2021-12-21 18:18:00 -0500

    added server and routes for post creating/rendering

---
+++ server/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Headers', "*")
+    res.setHeader('Access-Control-Allow-Methods',
+    "GET, POST, PATCH, DELETE, OPTIONS");
+    next();
+})
+
+app.post('/api/posts', (req,res,next)=>{
+    const post = req.body
+    console.log(post);
+    res.status(201).json();
+})
+
+app.get('/api/posts', (req,res) => {
+    const posts = [
+        {
+            id: 'jdlakfda', 
+            title: 'first server post', 
+            content: 'i made dis'
+        },
+        {
+            id: 'afdasfsaf', 
+            title: 'second server post', 
+            content: 'i made dis again'
+        },
+
+    ];
+    res.status(200).json({
+        message: "heres some posts, have fun",
+        posts: posts
+    })
+});
+
+
+
+module.exports = app;
